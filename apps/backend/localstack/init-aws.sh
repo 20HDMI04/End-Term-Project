a336b8fe42200fa595251a673b6aad216827e708
20HDMI04 <heropista@gmail.com>
2026-02-25 15:12:16 +0100

    fix: streamline bucket policy commands in init-aws.sh for improved readability

--- apps/backend/localstack/init-aws.sh
+++ apps/backend/localstack/init-aws.sh
@@ -15,9 +15,7 @@ awslocal s3 mb s3://$BUCKET_NAME 2>/dev/null
 
 # Setting policy for the bucket
 echo "Setting Public Read policy for $BUCKET_NAME"
-awslocal s3api put-bucket-policy \
-    --bucket $BUCKET_NAME \
-    --policy '{ "Version": "2012-10-17", "Statement": [ { "Sid": "PublicReadGetObject", "Effect": "Allow", "Principal": "*", "Action": "s3:GetObject", "Resource": "arn:aws:s3:::'"$BUCKET_NAME"'/*" } ] }'
+awslocal s3api put-bucket-policy \ --bucket $BUCKET_NAME \ --policy '{ "Version": "2012-10-17", "Statement": [ { "Sid": "PublicReadGetObject", "Effect": "Allow", "Principal": "*", "Action": "s3:GetObject", "Resource": "arn:aws:s3:::'"$BUCKET_NAME"'/*" } ] }'
 
 # Secondary bucket creation
 echo "Creating S3 bucket: $SECONDARY_BUCKET_NAME"
@@ -25,9 +23,7 @@ awslocal s3 mb s3://$SECONDARY_BUCKET_NAME 2>/dev/null
 
 # Setting policy for the secondary bucket
 echo "Setting Public Read policy for $SECONDARY_BUCKET_NAME"
-awslocal s3api put-bucket-policy \
-    --bucket $SECONDARY_BUCKET_NAME \
-    --policy '{ "Version": "2012-10-17", "Statement": [ { "Sid": "PublicReadGetObject", "Effect": "Allow", "Principal": "*", "Action": "s3:GetObject", "Resource": "arn:aws:s3:::'"$SECONDARY_BUCKET_NAME"'/*" } ] }'
+awslocal s3api put-bucket-policy \ --bucket $SECONDARY_BUCKET_NAME \ --policy '{ "Version": "2012-10-17", "Statement": [ { "Sid": "PublicReadGetObject", "Effect": "Allow", "Principal": "*", "Action": "s3:GetObject", "Resource": "arn:aws:s3:::'"$SECONDARY_BUCKET_NAME"'/*" } ] }'
 
 # Third bucket creation
 echo "Creating S3 bucket: $THIRD_BUCKET_NAME"
@@ -35,9 +31,7 @@ awslocal s3 mb s3://$THIRD_BUCKET_NAME 2>/dev/null
 
 # Setting policy for the third bucket
 echo "Setting Public Read policy for $THIRD_BUCKET_NAME"
-awslocal s3api put-bucket-policy \
-    --bucket $THIRD_BUCKET_NAME \
-    --policy '{ "Version": "2012-10-17", "Statement": [ { "Sid": "PublicReadGetObject", "Effect": "Allow", "Principal": "*", "Action": "s3:GetObject", "Resource": "arn:aws:s3:::'"$THIRD_BUCKET_NAME"'/*" } ] }'
+awslocal s3api put-bucket-policy \ --bucket $THIRD_BUCKET_NAME \ --policy '{ "Version": "2012-10-17", "Statement": [ { "Sid": "PublicReadGetObject", "Effect": "Allow", "Principal": "*", "Action": "s3:GetObject", "Resource": "arn:aws:s3:::'"$THIRD_BUCKET_NAME"'/*" } ] }'
 
 echo "S3 initialization complete."
 
